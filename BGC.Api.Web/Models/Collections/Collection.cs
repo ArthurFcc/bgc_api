e75f0291fc750d3a1a48f639d8e86922d29a9e25
Arthur Faccio <109385422+ArthurFcc@users.noreply.github.com>
2025-05-28 15:02:28 -0300

    Collection entity and lastupdate

---
+++ BGC.Api.Web/Models/Collections/Collection.cs
@@ -0,0 +1,18 @@
+﻿using BGC.Api.Web.Models.Boardgames;
+using BGC.Api.Web.Models.Shared;
+
+namespace BGC.Api.Web.Models.Collections
+{
+    public class Collection : Entity
+    {
+        public byte[] Cover { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public IEnumerable<Boardgame> Boardgames { get; set; }
+        
+        /*
+         * Future implementation of miniature collection
+         * public IEnumerable<Miniature> Miniatures { get; set; }
+        */
+    }
+}
--- BGC.Api.Web/Models/Shared/Entity.cs
+++ BGC.Api.Web/Models/Shared/Entity.cs
@@ -4,5 +4,10 @@
     {
         public uint Id { get; set; }
         public DateTime CreationTime { get; set; }
+        public DateTime LastUpdateTime { get; set; }
+
+        /*
+         * Implement user information to filter in the future
+         */
     }
 }
