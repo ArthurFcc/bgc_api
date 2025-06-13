dde6e5b9e8a1b921a2ca0dfeb3a6145d04a0031d
Arthur Faccio <109385422+ArthurFcc@users.noreply.github.com>
2025-06-13 15:05:54 -0300

    Collection filtered query

--- BGC.Api.Web/Controllers/Collections/CollectionController.cs
+++ BGC.Api.Web/Controllers/Collections/CollectionController.cs
@@ -1,7 +1,11 @@
 ﻿using BGC.Api.Web.Controllers.AsyncCrudController;
+using BGC.Api.Web.Controllers.Extensions;
 using BGC.Api.Web.Data;
 using BGC.Api.Web.Models.Collections;
+using BGC.Api.Web.Models.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Nelibur.ObjectMapper;
 
 namespace BGC.Api.Web.Controllers.Collections
@@ -11,6 +15,13 @@ namespace BGC.Api.Web.Controllers.Collections
     public class CollectionController(BGCDbContext dbContext)
         : AsyncCrudController<Collection, CollectionBase, GetCollection>(dbContext)
     {
+        public override IQueryable<Collection> CreateFilteredQuery(PagedRequest input)
+        {
+            return Context.Set<Collection>()
+                .Include(x => x.Boardgames)
+                .WhereIf(!input.SearchText.IsNullOrEmpty(), x => x.Title.Contains(input.SearchText!));
+        }
+
         public override Task<ActionResult<Collection>> Create(CollectionBase entity)
         {
             TinyMapper.Bind<CollectionBase, Collection>();
